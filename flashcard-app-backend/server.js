fa1822412e4ff15493eeba1f77176cf8d31cfa55
Khadka <120006828+khadka27@users.noreply.github.com>
2025-02-17 08:01:46 +0545

    Add dotenv dependency and configure environment variables in server

--- flashcard-app-backend/server.js
+++ flashcard-app-backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const flashcardRoutes = require('./routes/flashcards');
+require('dotenv').config();
+
 
 const app = express();
 
